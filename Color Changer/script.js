d5d2b1d0de17080ade46de636ee402444de944d2
RoshanrksKrSingh <roshanksingh94@gmail.com>
2024-10-14 16:00:57 +0530

    Update script.js

--- Color Changer/script.js
+++ Color Changer/script.js
@@ -1,1 +1,20 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 
+buttons.forEach( function(button){
+    button.addEventListener("click",function (e) {
+         if(e.target.id==='grey'){
+            // body.style.backgroundColor='grey';
+            body.style.backgroundColor= e.target.id;
+         }
+         if(e.target.id==='white'){
+            body.style.backgroundColor='white';
+         }
+         if(e.target.id==='red'){
+            body.style.backgroundColor='red';
+         }
+         if(e.target.id==='blue'){
+            body.style.backgroundColor='blue';
+         }   
+    })
+})
